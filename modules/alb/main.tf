b2a2d20116fc0c0332588292e441c84257bc5421
ZachamyrAutomator <zachamyr.automator@pm.me>
2025-09-03 12:44:07 -0700

    corrected trivy-action version and some linting.

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
+
 # Application Load Balancer
 resource "aws_lb" "main" {
   name               = "${var.name}-alb"
